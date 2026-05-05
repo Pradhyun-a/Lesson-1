1def87b29c27e2776bbf83752654b7a897f1f389
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-05-05 19:50:24 +1000

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,7 @@
+def weather(spring, autumn):
+    print("The weather is pleasent in ", spring )
+    print("The weather is cold in ", autumn )
+
+spring = "spring"
+autumn = "autumn"
+weather(spring, autumn)
