fae32be8f05ae3f6fa41d6538fbb7c3b5eaa281d
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-06-15 22:08:10 +1000

    Create trigValue.py

---
+++ trigValue.py
@@ -0,0 +1,9 @@
+import math
+
+degrees = float(input("Enter angle in degrees: "))
+
+radians = math.radians(degrees)
+
+print("Sin:", math.sin(radians))
+print("Cos:", math.cos(radians))
+print("Tan:", math.tan(radians))
