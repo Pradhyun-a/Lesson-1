e2d1f32664838720dfc5079f40307cf0b6d7f5d0
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-06-18 19:41:43 +1000

    Create test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+try:
+    print("1. add")
+    print("2. subtract")
+    print("3. multiply")
+    print("4. divide")
+
+value = int(input("Enter a value: "))
+
+execpt ValueError: 
+print("Enter a correct value")
+
+except ZeroDivisionError:
+print("Enter a correct value")
+
+print("1. add")
+print("2. subtract")
+print("3. multiply")
+print("4. divide")
