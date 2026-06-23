ef80adb9fcc22d18650a2a9ccc6bf2a7f83754eb
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-06-23 19:46:12 +1000

    Create listsPlay.py

---
+++ listsPlay.py
@@ -0,0 +1,10 @@
+num = [20, 30, 40, 50, 60, 70]
+total = sum(num)
+average = total / len(num)
+largest = max(num)
+smallest = min(num)
+
+print(total)
+print(average)
+print(largest)
+print(smallest)
