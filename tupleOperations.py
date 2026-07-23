1ea9019e1b83741e243bb9b3dcf2ca7dd9444436
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-07-23 19:41:14 +1000

    Create tupleOperations.py

---
+++ tupleOperations.py
@@ -0,0 +1,12 @@
+data = (2, "car", True, 4.6)
+print(data)
+
+numbers = (4, 5, 6, 7, 8)
+print(numbers)
+
+numbers = numbers + (9,)
+print(numbers)
+
+print(numbers.count(4))
+
+print(numbers[1 : 3])
