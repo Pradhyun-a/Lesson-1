b35087c23ce6bd93c75b877ae3b3165590a2ab07
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-07-30 19:38:47 +1000

    Create frequency.py

---
+++ frequency.py
@@ -0,0 +1,11 @@
+codingal = {'Codingal' : 2,
+    'is' : 2,
+    'best' : 2,
+      'for' : 2,
+        'Coding' : 1
+}
+
+frequency = 2
+for word, count in codingal.items():
+    if count == frequency:
+        print(word)
