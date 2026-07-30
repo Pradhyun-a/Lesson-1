49caba40d1f89132e10586f22ac2631196ed1eb1
Pradhyun-a <pradhyun.pradhyun17@gmail.com>
2026-07-30 19:38:06 +1000

    Create duplicates.py

---
+++ duplicates.py
@@ -0,0 +1,14 @@
+students = {
+
+101: {"name": "Alice", "class": 9, "subject": "Math"},
+
+102: {"name": "Bob", "class": 9, "subject": "Science"},
+
+103: {"name": "Charlie", "class": 9, "subject": "English"},
+
+102: {"name": "Bob", "class": 9, "subject": "Science"}
+
+}
+
+for id, details in students.items():
+    print(id, ":",details)
